9ca9791f3d14f4654569d15bd6bafd6f323c69e4
John Einselen <82116640+jeinselen@users.noreply.github.com>
2026-05-29 18:36:07 -0400

    Adding icon

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-APP_NAME="OpenCodeBalance"
+APP_NAME="MenubarBalance"
+ICON_NAME="MenubarBalance"
 APP_DIR="$APP_NAME.app"
 MACOS_DIR="$APP_DIR/Contents/MacOS"
 RESOURCES_DIR="$APP_DIR/Contents/Resources"
@@ -11,6 +12,16 @@ echo "Building $APP_NAME..."
 # Create directories
 mkdir -p "$MACOS_DIR"
 mkdir -p "$RESOURCES_DIR"
+
+# Compile the .icon asset (Icon Composer) into Assets.car + .icns
+xcrun actool "$ICON_NAME.icon" \
+    --compile "$RESOURCES_DIR" \
+    --platform macosx \
+    --minimum-deployment-target 26.0 \
+    --app-icon "$ICON_NAME" \
+    --output-partial-info-plist "$RESOURCES_DIR/.icon-partial.plist" \
+    --output-format human-readable-text \
+    --errors --warnings
 
 # Write Info.plist
 cat <<EOF > "$APP_DIR/Contents/Info.plist"
@@ -28,11 +39,17 @@ cat <<EOF > "$APP_DIR/Contents/Info.plist"
     <string>1.0</string>
     <key>CFBundleShortVersionString</key>
     <string>1.0</string>
+    <key>CFBundleIconFile</key>
+    <string>$ICON_NAME</string>
+    <key>CFBundleIconName</key>
+    <string>$ICON_NAME</string>
     <key>LSUIElement</key>
     <true/>
 </dict>
 </plist>
 EOF
+
+rm -f "$RESOURCES_DIR/.icon-partial.plist"
 
 # Compile Swift code
 swiftc Sources/main.swift Sources/AppDelegate.swift Sources/Providers.swift Sources/ProvidersWindowController.swift -o "$MACOS_DIR/$APP_NAME"
